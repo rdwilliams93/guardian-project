d207466107ea63a33d1e95118e458f76786e0262
PiriAmalan <piri.amalan@gmail.com>
2014-06-27 15:35:26 +0100

    Update site.js

--- site.js
+++ site.js
@@ -1,22 +1,30 @@
  $(document).ready(function() {
     var list={"1":"uk-news","2":"football","3":"travel"};
+
+
     $.each(list, function(key, value) {
       $.ajax({
-        url: "http://content.guardianapis.com/search?section=" + value + "&show-fields=trailText",
+        url: "http://content.guardianapis.com/search?section=" + value + "&show-fields=trailText%2Cthumbnail",
         dataType: "jsonp",
 
         success: function(data) {
           $.each(data.response.results, function (i) {
-            $( "#" + value ).append('<h5><a href="' + this['webUrl'] + '">' + this['webTitle'] + '</a></h5>');
+            $( "#" + value ).append('<h4><a href="' + this['webUrl'] + '">' + this['webTitle'] + '</a></h4>');
             $( "#" + value ).append("<li>" + this['fields'].trailText + "</li>");
+             $( "#" + value ).append("<img src = " + this['fields'].thumbnail + ">");
             if ( i === 4 ) {
             return false;}
           });
         }});
     });
-  });
-			
-	$(function() {
-		$( "#tabs" ).tabs();
-	  });
+  };
 
+    });
+  });
+
+
+
+}
+  $(function() {
+    $( "#tabs" ).tabs();
+    });
