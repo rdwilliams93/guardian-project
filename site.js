c084b306caf193ffb3f631ceb356cb37e382a971
mike <mginz@users.noreply.github.com>
2014-06-27 15:56:21 +0100

    Update site.js

--- site.js
+++ site.js
@@ -1,3 +1,7 @@
+if(screen.width>500){
+
+
+
  $(document).ready(function() {
     var list={"1":"uk-news","2":"football","3":"travel"};
     $.each(list, function(key, value) {
@@ -16,6 +20,29 @@
         }});
     });
   });
+
+}
+
+else{
+ $(document).ready(function() {
+    var list={"1":"uk-news","2":"football","3":"travel"};
+    $.each(list, function(key, value) {
+      $.ajax({
+        url: "http://content.guardianapis.com/search?section=" + value + "&show-fields=trailText",
+        dataType: "jsonp",
+
+        success: function(data) {
+          $.each(data.response.results, function (i) {
+            $( "#" + value ).append('<li><a href="' + this['webUrl'] + '">' + this['webTitle'] + '</a></li>');
+            $( "#" + value ).append("<p>" + this['fields'].trailText + "</p>");
+            if ( i === 4 ) {
+            return false;}
+          });
+        }});
+    });
+  });
+
+}
 
 $(function() {
     $( "#tabs" ).tabs();
